ad589c3377c4ff30b06f67d6146fa4cd48246b08
Erdle, Tobias <tobias.erdle@innoq.com>
2019-08-07 11:13:25 +0200

    Add more JavaDoc to QueryParamLocaleResolver

--- custom-locale-resolver/src/main/java/de/erdlet/mvc/QueryParamLocaleResolver.java
+++ custom-locale-resolver/src/main/java/de/erdlet/mvc/QueryParamLocaleResolver.java
@@ -7,8 +7,21 @@ import javax.mvc.locale.LocaleResolver;
 import javax.mvc.locale.LocaleResolverContext;
 
 /**
- * Resolver to get the {@link Locale} to use from the requests query param.
+ * Resolver to get the {@link Locale} to use from the requests query param <i>lang</i>.
  *
+ * In case there is no request param with this name, the {@link Locale} will be resolved
+ * by a higher prioritised implementation.
+ *
+ * Example usage:
+ * <pre>
+ * {@code
+ * # Use default locale
+ * curl <your-url>
+ *
+ * # Set german locale by query param
+ * curl <your-url>?lang=de-DE
+ * }
+ * </pre>
  * @author Tobias Erdle
  */
 @Priority(1)
